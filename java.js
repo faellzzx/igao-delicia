5f38f8caa4d415206405dfb2e7a0a0c376a308c9
faellzzx <batata1313banana@gmail.com>
2026-06-25 16:10:00 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,6 @@
+function Soma()
+{
+var e1 = parseInt(document.getElementById("v1").value);
+var e2 = parseInt(document.getElementById("v2").value);
+document.getElementById("res").innerHTML = "Resposta: " + (e1 + e2);
+}
